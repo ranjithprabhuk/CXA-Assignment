be75f2df87a620c6a94539976e1747861a48accf
Ranjithprabhu K <ranjithprabhu.k@gmail.com>
2018-05-11 15:47:45 +0530

    updated the api key

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,12 @@
+const flickrConfig = {
+    apiKey: '12aaca691f3effb2e4ec52478e110197',
+    method: 'flickr.photos.getRecent',
+}
+
 export const AppConfig = {
     appTitle: 'CXA Group - Case Assignment Application',
     // tslint:disable-next-line
-    flickrApiBase: 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=b92a39ddacaa1916cd1bde54db89e17a&format=json&nojsoncallback=1',
+    flickrApiBase: `https://api.flickr.com/services/rest/?method=${flickrConfig.method}&api_key=${flickrConfig.apiKey}&format=json&nojsoncallback=1`,
     toastrConfig: {
         timeOut: 4000,
         positionClass: 'toast-bottom-right',
